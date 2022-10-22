779b035c9069ace55e446b12898fbc9a44bdfd10
eaharris <harris.2189@osu.edu>
2022-10-22 09:17:01 -0400

    Adding all the files at once.

---
+++ download-data.R
@@ -0,0 +1,6 @@
+library(dslabs)
+library(tidyverse)
+
+url <- "https://raw.githubusercontent.com/rafalab/dslabs/master/inst/extdata/murders.csv"
+dest_file <- "data/murders.csv"
+download.file(url, destfile = dest_file)
